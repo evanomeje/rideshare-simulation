5891c62517f974be3e8404a323c78f28e76648d9
chima <evanomeje@gmail.com>
2025-01-04 14:56:46 -0700

    Build frontend for production

--- main.go
+++ main.go
@@ -6,8 +6,8 @@ import (
 	"log"
 	"net/http"
 	"os"
-  
-  _ "fmt" 
+
+	_ "fmt"
 
 	"github.com/joho/godotenv"
 )
@@ -82,8 +82,7 @@ func main() {
 	}
 	serverEnv := os.Getenv("SERVER_ENV")
 
-
-  log.Printf("Starting server in %s mode on port %s", serverEnv, serverPort)
+	log.Printf("Starting server in %s mode on port %s", serverEnv, serverPort)
 
 	var err error
 	if serverEnv == "PROD" {
